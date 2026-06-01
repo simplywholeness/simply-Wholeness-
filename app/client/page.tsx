7977d7931072feb30473936dfdf1e43d402f9f28
simplywholeness <hadassah@simplywholeness.com>
2026-06-01 10:21:34 -0400

    Update page.tsx

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
 import { redirect } from "next/navigation";
 import { BookOpen, CalendarCheck, HeartPulse, ListChecks, Target, TrendingUp } from "lucide-react";
 import { AppShell } from "@/components/AppShell";
